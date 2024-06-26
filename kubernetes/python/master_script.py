9e6e9d4117804027e35d03c765bb1c7f473c225e
petrosmp <petrakis798@gmail.com>
2024-06-26 20:44:34 +0000

    python wip

--- kubernetes/python/master_script.py
+++ kubernetes/python/master_script.py
@@ -1,10 +1,12 @@
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
+from kubernetes.client.models import V1Pod
 import os
 
 NAMESPACE = "torpili"
 NUM_SPLITTERS = 1
-
+INPUT_FILE = "/mnt/longhorn/input_file"
+NUM_MAPPERS = 3
 
 def create_pods():
     config.load_incluster_config()
@@ -19,8 +21,8 @@ def create_pods():
                 "image": "georgestav/splitter:latest",
                 "command": ["python", "splitter.py"],
                 "args": [
-                    "worerw\n efwf w\n 23r ef\n",
-                    "3"
+                    INPUT_FILE,
+                    str(NUM_MAPPERS)
                 ],
                 "volumeMounts": [
                     {
@@ -82,9 +84,95 @@ def create_pods():
     }
 
 
+
+    mapper_spec_dict = {
+        "containers": [
+            {
+                "name": "mapper",
+                "image": "georgestav/mapper:latest",
+                "command": ["python", "mapper.py"],
+                "args": [
+                    "/mnt/longhorn/split0.txt"
+                ],
+                "volumeMounts": [
+                    {
+                        "mountPath": "/mnt/longhorn",
+                        "name": "longhorn-storage"
+                    }
+                ]
+            }
+        ],
+        "volumes": [
+            {
+                "name": "longhorn-storage",
+                "persistentVolumeClaim": {
+                    "claimName": "longhorn-pvc"
+                }
+            }
+        ]
+    }
+
+
+    mapper_statefulset_manifest = {
+        "apiVersion": "apps/v1",
+        "kind": "StatefulSet",
+        "metadata": {
+            "name": "mapper",
+            "namespace": "torpili"
+        },
+        "spec": {
+            "serviceName": "mapper-service",
+            "podManagementPolicy": "Parallel",
+            "replicas": NUM_MAPPERS,
+            "selector": {
+                "matchLabels": {
+                    "app": "mapper"
+                }
+            },
+            "template": {
+                "metadata": {
+                    "labels": {
+                        "app": "mapper"
+                    }
+                },
+                "spec": mapper_spec_dict
+            }
+        }
+    }
+
+    splitter_service_manifest = {
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {
+            "name": "mapper-service"
+        },
+        "spec": {
+            "selector": {
+                "app": "mapper"
+            },
+            "type": "ClusterIP"
+        }
+    }
+
     try:
+
+        # create splitters
         core_api.create_namespaced_service(NAMESPACE, splitter_service_manifest)
         apps_api.create_namespaced_stateful_set(NAMESPACE, splitter_statefulset_manifest)
+
+        splitters: list[V1Pod] = core_api.list_namespaced_pod(NAMESPACE, label_selector="app=splitter").items
+
+        split_done = False
+
+        while not split_done:
+            for i in splitters:
+                if i: break
+
+        # create mappers
+        core_api.create_namespaced_service(NAMESPACE, splitter_service_manifest)
+        apps_api.create_namespaced_stateful_set(NAMESPACE, splitter_statefulset_manifest)
+        
+
 
         print("SplitterStatefulSet created successfully")
     except ApiException as e:
