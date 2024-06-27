e542d4d561663f889dfa9dff876ff26396272400
petrosmp <petrakis798@gmail.com>
2024-06-27 05:27:58 +0300

    shuffler routing

--- kubernetes/python/master_script.py
+++ kubernetes/python/master_script.py
@@ -4,11 +4,11 @@ from kubernetes.client.models import V1Pod
 import os
 
 NAMESPACE = "torpili"
-NUM_SPLITTERS = 1
 INPUT_FILE = os.getenv("INPUT_FILE", "/mnt/longhorn/input_file")
+NUM_SPLITTERS = 1
+NUM_SHUFFLERS = 1
 NUM_MAPPERS = int(os.getenv("NUM_MAPPERS", 3))
 NUM_REDUCERS = int(os.getenv("NUM_REDUCERS", 5))
-
 
 def create_pods():
     config.load_incluster_config()
@@ -91,6 +91,48 @@ def create_pods():
         "spec": {"selector": {"app": "mapper"}, "type": "ClusterIP", "ports": [{"port": 7777, "targetPort": 7777}]}
     }
 
+
+    shuffler_spec_dict = {
+        "containers": [
+            {
+                "name": "shuffler",
+                "image": "georgestav/shuffler:latest",
+                "env": [
+                    {
+                        "name": "NUM_REDUCERS",
+                        "value": NUM_REDUCERS
+                    }
+                ],
+                "command": ["python", "shuffler.py"],
+                "volumeMounts": [{"mountPath": "/mnt/longhorn", "name": "longhorn-storage"}],
+            }
+        ],
+        "volumes": [{"name": "longhorn-storage", "persistentVolumeClaim": {"claimName": "longhorn-pvc"}}],
+    }
+
+    shuffler_statefulset_manifest = {
+        "apiVersion": "apps/v1",
+        "kind": "StatefulSet",
+        "metadata": {"name": "shuffler", "namespace": "torpili"},
+        "spec": {
+            "serviceName": "shuffler-service",
+            "podManagementPolicy": "Parallel",
+            "replicas": NUM_SHUFFLERS,
+            "selector": {"matchLabels": {"app": "shuffler"}},
+            "template": {"metadata": {"labels": {"app": "shuffler"}}, "spec": shuffler_spec_dict},
+        },
+    }
+
+    shuffler_service_manifest = {
+        "apiVersion": "v1",
+        "kind": "Service",
+        "metadata": {"name": "shuffler-service"},
+        "spec": {"selector": {"app": "shuffler"}, "type": "ClusterIP", "ports": [{"port": 7777, "targetPort": 7777}]}
+    }
+
+
+
+
     try:
 
         # create splitters
@@ -135,7 +177,25 @@ def create_pods():
             break
         print(f"done mapping")
 
-        print("Split & Map completed successfully")
+
+        # create shufflers
+        core_api.create_namespaced_service(NAMESPACE, shuffler_service_manifest)
+        apps_api.create_namespaced_stateful_set(NAMESPACE, shuffler_statefulset_manifest)
+
+        mappers: list[V1Pod] = core_api.list_namespaced_pod(NAMESPACE, label_selector="app=shuffler").items
+
+        # loop untill all shufflers have completed
+        i = 0
+        while True:
+            while i < len(mappers):
+                if mappers[i].status.phase == "Completed":
+                    mappers.pop(i)
+                    i += 1
+                    break
+            break
+        print(f"done shuffling")
+
+        print("Split & Map & Shuffle completed successfully")
 
         apps_api.delete_namespaced_stateful_set(namespace=NAMESPACE, name="splitter")
         apps_api.delete_namespaced_stateful_set(namespace=NAMESPACE, name="mapper")
